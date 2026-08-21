753a391ed5cd8ab0b63878ec853b33ba3898d29f
Pawan Upadhyay <141557022+pawansatoshi@users.noreply.github.com>
2026-08-21 11:04:34 +0530

    chore: remove temporary lockfile export helper

--- scripts/export-lock.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs'
-
-fs.mkdirSync('public', { recursive: true })
-if (!fs.existsSync('package-lock.json')) throw new Error('package-lock.json was not created by npm install')
-fs.copyFileSync('package-lock.json', 'public/package-lock.json')
